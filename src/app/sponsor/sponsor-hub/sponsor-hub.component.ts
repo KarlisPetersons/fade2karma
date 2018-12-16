5f37e8cad15fc3c75f2adb4207cbd825976bb4b0
Karlis Petersons <karlis039@gmail.com>
2018-12-16 11:08:43 +0100

    Ample removed from sponsors page and Operaevent added

--- src/app/sponsor/sponsor-hub/sponsor-hub.component.ts
+++ src/app/sponsor/sponsor-hub/sponsor-hub.component.ts
@@ -13,18 +13,27 @@ export class SponsorHubComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {
+        // this.sponsors.push(new Sponsor({
+        //     imageURL: 'assets/sponsor-images/rush49.png',
+        //     link: 'https://rush49.com/',
+        //     links: [{
+        //         url: 'https://www.facebook.com/Rush49',
+        //         icon: 'assets/icons/facebook.svg'
+        //     }, {
+        //         url: 'https://twitter.com/Rush49',
+        //         icon: 'assets/icons/twitter.svg'
+        //     }, {
+        //         url: 'https://www.youtube.com/user/Rush49deals',
+        //         icon: 'assets/icons/youtube.svg'
+        //     }]
+        // }));
+
         this.sponsors.push(new Sponsor({
-            imageURL: 'assets/sponsor-images/rush49.png',
-            link: 'https://rush49.com/',
+            imageURL: 'assets/sponsor-images/quest-mode.png',
+            link: 'https://gather.operaevent.co/',
             links: [{
-                url: 'https://www.facebook.com/Rush49',
-                icon: 'assets/icons/facebook.svg'
-            }, {
-                url: 'https://twitter.com/Rush49',
+                url: 'https://twitter.com/operaeventco',
                 icon: 'assets/icons/twitter.svg'
-            }, {
-                url: 'https://www.youtube.com/user/Rush49deals',
-                icon: 'assets/icons/youtube.svg'
             }]
         }));
 
@@ -42,6 +51,18 @@ export class SponsorHubComponent implements OnInit {
                 icon: 'assets/icons/youtube.svg'
             }]
         }));
+
+        // this.sponsors.push(new Sponsor({
+        //     imageURL: 'assets/sponsor-images/tayroc.jpeg',
+        //     link: 'https://tayroc.refersion.com/c/3eb57c',
+        //     links: [{
+        //         url: 'https://www.facebook.com/OfficialTayroc/',
+        //         icon: 'assets/icons/facebook.svg'
+        //     }, {
+        //         url: 'https://twitter.com/tayrocwatches',
+        //         icon: 'assets/icons/twitter.svg'
+        //     }]
+        // }));
 
         // this.sponsors.push(new Sponsor({
         //     imageURL: 'assets/sponsor-images/aftershockz.png',
