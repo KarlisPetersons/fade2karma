234657010113d39c5840e8e51df18cc016541b34
Karlis Petersons <karlis039@gmail.com>
2018-12-16 15:55:06 +0100

    sponsors: Questmode, Twitch, and Rush49.

--- src/app/sponsor/sponsor-hub/sponsor-hub.component.ts
+++ src/app/sponsor/sponsor-hub/sponsor-hub.component.ts
@@ -13,21 +13,6 @@ export class SponsorHubComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {
-        // this.sponsors.push(new Sponsor({
-        //     imageURL: 'assets/sponsor-images/rush49.png',
-        //     link: 'https://rush49.com/',
-        //     links: [{
-        //         url: 'https://www.facebook.com/Rush49',
-        //         icon: 'assets/icons/facebook.svg'
-        //     }, {
-        //         url: 'https://twitter.com/Rush49',
-        //         icon: 'assets/icons/twitter.svg'
-        //     }, {
-        //         url: 'https://www.youtube.com/user/Rush49deals',
-        //         icon: 'assets/icons/youtube.svg'
-        //     }]
-        // }));
-
         this.sponsors.push(new Sponsor({
             imageURL: 'assets/sponsor-images/quest-mode.png',
             link: 'https://gather.operaevent.co/',
@@ -38,19 +23,46 @@ export class SponsorHubComponent implements OnInit {
         }));
 
         this.sponsors.push(new Sponsor({
-            imageURL: 'assets/sponsor-images/ample.png',
-            link: 'https://www.amplemeal.com/',
+            imageURL: 'assets/sponsor-images/twitch.png',
+            link: 'https://www.twitch.tv/',
             links: [{
-                url: 'https://www.facebook.com/amplemeal/',
-                icon: 'assets/icons/facebook.svg'
-            }, {
-                url: 'https://twitter.com/amplemeal',
+                url: 'https://twitter.com/Twitch',
                 icon: 'assets/icons/twitter.svg'
             }, {
-                url: 'https://www.youtube.com/channel/UCARyuqkXBF6aOoxyHcsD-mA',
+                url: 'https://www.facebook.com/Twitch/',
+                icon: 'assets/icons/facebook.svg'
+            }]
+        }));
+
+        this.sponsors.push(new Sponsor({
+            imageURL: 'assets/sponsor-images/rush49.png',
+            link: 'https://rush49.com/',
+            links: [{
+                url: 'https://www.facebook.com/Rush49',
+                icon: 'assets/icons/facebook.svg'
+            }, {
+                url: 'https://twitter.com/Rush49',
+                icon: 'assets/icons/twitter.svg'
+            }, {
+                url: 'https://www.youtube.com/user/Rush49deals',
                 icon: 'assets/icons/youtube.svg'
             }]
         }));
+
+        // this.sponsors.push(new Sponsor({
+        //     imageURL: 'assets/sponsor-images/ample.png',
+        //     link: 'https://www.amplemeal.com/',
+        //     links: [{
+        //         url: 'https://www.facebook.com/amplemeal/',
+        //         icon: 'assets/icons/facebook.svg'
+        //     }, {
+        //         url: 'https://twitter.com/amplemeal',
+        //         icon: 'assets/icons/twitter.svg'
+        //     }, {
+        //         url: 'https://www.youtube.com/channel/UCARyuqkXBF6aOoxyHcsD-mA',
+        //         icon: 'assets/icons/youtube.svg'
+        //     }]
+        // }));
 
         // this.sponsors.push(new Sponsor({
         //     imageURL: 'assets/sponsor-images/tayroc.jpeg',
